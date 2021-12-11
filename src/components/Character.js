e08d3ef1d4b08194bfa8ce08994c243cc7e01d03
Hailey Magoline <magoline.hailey@gmail.com>
2021-12-10 18:43:17 -0600

    added styled component elements to page.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,20 +1,35 @@
 // Write your Character component here
 import React from 'react'
-
 import styled from 'styled-components'
+
+const CharacterDiv = styled.div`
+    background-color: grey;
+    width: 50%;
+    margin: auto;
+    border-radius: 25px;
+h3{
+    text-shadow: 1px 1px 5px #fff;
+}
+button{
+    padding: 1%;
+    margin: 1%;
+    font-weight: bold;
+}
+
+`
 
 const Character = props => {
 
     const{ info, open } = props
    
     return (
-        <div className='character'>
-            {info.name}
+        <CharacterDiv>
+           <h3>{info.name}</h3>
             
             <button onClick ={open}>
                 See Details
             </button>
-        </div>
+        </CharacterDiv>
   )
 }
 
